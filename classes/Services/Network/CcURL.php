e96d329d7b3396c3a41b550b2d2995e29431d5e3
Nikolay <nikolay.b@tegra.media>
2018-12-15 20:09:15 +0300

    added cURL wrapper class

---
+++ classes/Services/Network/CcURL.php
@@ -0,0 +1,60 @@
+<?php
+namespace Services\Network;
+
+/**
+ * cURL wrapper class
+ */
+class CcURL
+{
+    /**
+     * @param string $url
+     * @param array $params
+     * 
+     * @return mixed
+     */
+    public static function post(string $url, array $params)
+    {
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_POSTFIELDS => $params,
+            CURLOPT_RETURNTRANSFER => true,
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $options);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        if ($result === false) {
+            return null;
+        } else {
+            return $result;
+        }
+    }
+
+    /**
+     * @param string $url
+     * @param array $params
+     * 
+     * @return mixed
+     */
+    public static function get(string $url, array $params)
+    {
+        $ecodedURL = $url .'?'. http_build_query($params);
+        $options = [
+            CURLOPT_URL => $ecodedURL,
+            CURLOPT_RETURNTRANSFER => true,
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $options);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        if ($result === false) {
+            return null;
+        } else {
+            return $result;
+        }
+    }
+}
