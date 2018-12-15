2d40fbe275d5bda073e562896a4f88cadb81ffe3
Nikolay <nikolay.b@tegra.media>
2018-12-16 01:18:42 +0300

    Base WoT Wargaming API class

--- classes/Entity/WargamingAPI/WoT/CBase.php
+++ classes/Entity/WargamingAPI/WoT/CBase.php
@@ -1,10 +1,42 @@
 <?php
-namespace Entity\WargamingStats;
+namespace Entity\WargamingAPI\WoT;
+
+use Services\Network\CcURL;
 
 abstract class CBase
 {
-    public static function _api()
+    protected $protocol = 'https';
+    protected $server = 'api.worldoftanks.ru';
+    protected $API_name = 'wot';
+    protected $method_block = '';
+
+    /**
+     * @param string $method_name Method for execution
+     * @param array $params Parameters for method
+     *
+     * @return mixed
+     */
+    protected function _api(string $method_name, array $params)
     {
-        print('hi');
+        $url = "{$this->protocol}://{$this->server}/{$this->API_name}/{$this->method_block}/{$method_name}/";
+        $result = CcURL::post($url, $params);
+
+        return $result;
+    }
+
+    /**
+     * @param array $options Options for method parameters
+     * 
+     * @return array
+     */
+    protected function _prepareParams(array $options) {
+        global $APPLICATION;
+        $defaults = [
+            'application_id' => $APPLICATION->getConfiguration('APP_ID'),
+            'language' => $APPLICATION->getConfiguration('LANGUAGE'),
+        ];
+
+        $result = array_merge($defaults, $options);
+        return $result;
     }
 }
