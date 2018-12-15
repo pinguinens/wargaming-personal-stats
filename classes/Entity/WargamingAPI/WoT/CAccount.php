447a65e4c191706684db5582a77b0f7c09284944
Nikolay <nikolay.b@tegra.media>
2018-12-16 01:19:15 +0300

    Base WoT Account class

--- classes/Entity/WargamingAPI/WoT/CAccount.php
+++ classes/Entity/WargamingAPI/WoT/CAccount.php
@@ -1,8 +1,26 @@
 <?php
-namespace Entity\WargamingStats\WoT;
+namespace Entity\WargamingAPI\WoT;
 
-use Entity\WargamingStats\CBase;
+use Entity\WargamingAPI\WoT\CBase;
 
 class CAccount extends CBase
 {
+    protected $method_block = 'account';
+
+    /**
+     * @param string $search Player nickname
+     */
+    public function getPlayer(string $search)
+    {
+        $method_name = 'list';
+        $options = [
+            'search' => $search,
+            'type' => 'exact',
+        ];
+        $params = $this->_prepareParams($options);
+
+        $result = $this->_api($method_name, $params);
+
+        return $result;
+    }
 }
