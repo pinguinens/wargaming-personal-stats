aeaa2e0fb86119da3d25d746190df033af196cbe
Nikolay <nikolay.b@tegra.media>
2018-12-22 23:36:53 +0300

    fix

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 /** Main configuration file */
 $_APP_ = [
-    'APP_ID' => '3ce8752d4d014f4d01c199666b8d1021',
+    'APP_ID' => 'xxxxxxxxxx',
     'LANGUAGE' => 'ru',
     'AUTH_REDIRECT_URI' => 'https://0.0.0.0',
 ];
